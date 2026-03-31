8ba0645b1f8d864bf6fec9efa4728edf97a43106
NiketDeole <niketdeole0@gmail.com>
2026-03-31 22:37:31 +0530

    updating

--- script1.sh
+++ script1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Script 1: System Identity Report
-# Author: Krishna (24BCE10678)
+# Author: Niket (24BCE10467)
 # Course: Open Source Software
 # Software Choice: Git
 
 # --- Variables ---
 # Specific details for audit 
-STUDENT_NAME="Krishna"
+STUDENT_NAME="Niket"
 SOFTWARE_CHOICE="Git"
 
 # --- System info ---
